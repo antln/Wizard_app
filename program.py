9b71e00c34a891c99b7275e30783c72d68890dd1
Antoine Launay <az.launay@hotmail.com>
2019-05-07 15:11:27 +0200

    Ended the project

--- program.py
+++ program.py
@@ -1,4 +1,8 @@
+# import actors.py
+import random
+import time
 
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
@@ -8,24 +12,58 @@ def main():
 
 def print_header():
     print('----------------------------------------')
-    print('----------WIZARD DAME APP---------------')
+    print('----------WIZARD GAME APP---------------')
     print('----------------------------------------')
     print()
 
+
 def game_loop():
+    creatures = [
+        SmallAnimal('Toad', 1),
+        Creature('Tiger', 12),
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000)
+
+    ]
+
+    # print(creatures)
+
+    hero = Wizard('Gandalf', 75)
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appear from a dark and foggy forest...'.format(active_creature.name,
+                                                                                   active_creature.level))
+
         cmd = input('Do you [a]ttack, [r]runaway, or [l]ook around? ')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover")
+                time.sleep(5)
+                print("The wizard returns revitalized")
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard has become unsure of his power and flees.')
         elif cmd == 'l':
-            print('lookaround')
+            print('The wizard {} takes in the surrounding and sees:'.format(hero.name))
+            for c in creatures:
+                print(' * A {} of level {}'.format(
+                    c.name, c.level
+                ))
+
         else:
             print('Ok, exiting game.... bye !')
             break
+
+
+        if not creatures:
+            print("You defeated all the creatures.")
+            break
+
+        print()
 
 
 if __name__ == '__main__':
